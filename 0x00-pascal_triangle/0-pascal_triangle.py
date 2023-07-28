d4462d97bbbc09ea1a67cb3d73717b9869f005b7
kalwhyte <superkalel55@gmail.com>
2023-07-28 11:33:33 +0100

    cleared white space

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -14,8 +14,7 @@ def pascal_triangle(n):
     triangle = [[1]]
     for i in range(1, n):
         prev_row = triangle[-1]
-        new_row = [1]
-    
+        new_row = [1]    
         for j in range(1, len(prev_row)):
             new_row.append(prev_row[j - 1] + prev_row[j])
 
