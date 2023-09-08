23a8cb743e281b4effe7cf912a027f88b732278c
kalwhyte <superkalel55@gmail.com>
2023-09-08 02:08:58 +0100

    This script takes a Movie ID as a command-line argument, fetches movie data from the Star Wars API, and then fetches and prints the character names in the order provided by the API.

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// Prints all characters of a Star Wars movie
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      request(character, function (err, response, body) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
