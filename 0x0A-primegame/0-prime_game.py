fc169d4fe9464b87695f6491eada6238933af6a1
kalwhyte <superkalel55@gmail.com>
2023-10-05 17:38:01 +0100

    prime

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,31 +4,21 @@ from math import sqrt
 
 
 def isWinner(x, nums):
-    def is_prime(num):
-        if num < 2:
-            return False
-        for i in range(2, int(sqrt(num)) + 1):
-            if num % i == 0:
-                return False
-        return True
-    
-    def get_primes(n):
-        if n <= 2:
-            return True
-        return n % 2 == 0
-    
-    winner_count = {"Maria": 0, "Ben": 0}
-
-    for n in nums:
-        if get_primes(n):
-            winner_count["Maria"] += 1
-        else:
-            winner_count["Ben"] += 1
-
-    if winner_count["Maria"] > winner_count["Ben"]:
-        return "Maria"
-    elif winner_count["Maria"] < winner_count["Ben"]:
-        return "Ben"
-    else:
+    """ prime game """
+    if not nums or x < 1:
         return None
-    
+    Maria = 0
+    ben = 0
+    # get primes up to max num
+    n = max(nums)
+    primes = set(range(2, n + 1))
+    for i in range(2, int(n**5)+ 1):
+        primes -= set(range(i * i, n + 1, i))
+   # count primes for each player
+    for n in nums[:x]:
+        primes_count = sum([p <= n for p in primes])
+        ben += primes_count % 2 == 0
+        Maria += primes_count % 2 == 1
+    if Maria == ben:
+        return None
+    return 'Maria' if Maria > ben else 'Ben'
